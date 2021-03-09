70d59f77006ab9fb7ffda6df9f2a8d3e4f0db2bb
IamSanketMore <sanketmore.999.m@gmail.com>
2021-03-09 15:12:08 +0530

    Test Case for Check queue all element are stored

---
+++ test/java/MyQueueTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyQueueTest {
+    MyQueue myQueue=new MyQueue();
+    @Test
+    public void givenThreeNoWhenAddedToQueueShouldReturnTrue() {
+        MyNode<Integer> myFirstNode=new MyNode<>(56);
+        MyNode<Integer> mySecondNode=new MyNode<>(30);
+        MyNode<Integer> myThirdNode=new MyNode<>(70);
+        myQueue.queue(myFirstNode);
+        myQueue.printQueue();
+        myQueue.queue(mySecondNode);
+        myQueue.printQueue();
+        myQueue.queue(myThirdNode);
+        myQueue.printQueue();
+        INode peak=myQueue.peak();
+        Assertions.assertEquals(peak, myThirdNode);
+    }
+}
